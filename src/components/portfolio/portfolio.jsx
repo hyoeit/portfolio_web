992fae02f832b63f0aaa81da09649be1e41ea38d
Hyoe <gytmd0221@gmail.com>
2021-09-08 21:57:24 +0900

    portfolio css

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -10,22 +10,13 @@ const Portfolio = () => {
       <h2>portfolio</h2>
       <section className={styles.noteSection}>
         <img className={styles.noteImg} src="image/note.png" alt="note" />
-        <div>
+        <div className={styles.note} >
           <a className={styles.noteLink} href="https://hyoeit.github.io/todolist_withoutLogin/" target="_blank">1. Note</a>
-          <p> 오프라인에서 자주 사용하는 노트를 참고하여 만든 메모장 입니다. </p>
-          <p> 할 일 추가, 삭제, 체크, 중요표시 기능을 동적으로 만들었습니다. </p>
-          <p> 추 후 Firebase로 로그인 연동을 시킬 예정입니다.</p>
-          <div>
-            <p>CATEGORY</p>
-            <p>SOURCE</p>
-          </div>
-          <div>
-            <img className={styles.reactIcon} src="image/react.png" alt="react" />
-            <p>REACT</p>
-            <p>GITHUB</p>
-          </div>
+          <p className={styles.desc} >         오프라인에서 자주 사용하는 노트를 참고하여 만든 메모장. </p>
+          <p className={styles.desc}> 할 일 추가, 삭제, 체크, 중요표시 기능을 동적으로 만들었습니다. </p>
+          <p className={styles.skills}>사용한기술 : REACT, JAVASCRIPT, CSS</p>
+          <a className={styles.github} href="https://github.com/hyoeit/todolist_withoutLogin">GITHUB 바로가기</a>
 
-          
         </div>
         
       </section>
